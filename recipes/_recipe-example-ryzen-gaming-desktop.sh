0f7e162a1eb79c16b7853ce44598b32ff2971062
Robert <rstrube@gmail.com>
2020-09-03 15:27:32 -0600

    reorganized recipes

---
+++ recipes/_recipe-example-ryzen-gaming-desktop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Example soagi post-install recipe for Ryzen gaming desktop
+
+cd ../post_install
+
+# Core applications / utilities
+./core_yay.sh
+./core_utils.sh
+./core_zsh.sh
+
+# Web applications / utilities
+./web_chromium.sh --enable-vaapi
+
+# Development applications / utilities
+./dev_git.sh "Robert Strube" robert@mydomain.com --install-git-credential-manager
+
+# Media
+./media_codecs-player.sh
+
+# Icons / Fonts / Themes
+./gnome_icons.sh
+./gnome_fonts.sh --configure-vscode-fonts
+./gnome_theme-dracula.sh --configure-vscode-theme --configure-flatpak-theme
+
+# Gaming
+./gaming_steam.sh
---
+++ recipes/_recipe-example-sys76-laptop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Example soagi post-install recipe for System76 laptop (work machine)
+
+cd ../post_install
+
+# Core applications / utilities
+./core_yay.sh
+./core_utils.sh
+./core_zsh.sh
+./core_btrbk.sh /mnt/btr_root_vol btr_snapshots /mnt/btr_ext_ssd sys76
+
+# Web applications / utilities
+./web_chromium.sh --enable-vaapi
+./web_slack.sh
+./web_teams.sh
+
+# Development applications / utilities
+./dev_git.sh "Robert Strube" robert@mydomain.com --install-git-credential-manager
+./dev_vscode.sh
+
+# Media
+./media_codecs-player.sh
+
+# Icons / Fonts / Themes
+./gnome_icons.sh
+./gnome_fonts.sh --configure-vscode-fonts
+./gnome_theme-dracula.sh --configure-vscode-theme --configure-flatpak-theme
