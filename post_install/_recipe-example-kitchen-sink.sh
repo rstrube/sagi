ff0c668851d08ebece161df6cbaa879dd8237801
Robert <rstrube@gmail.com>
2020-09-02 19:07:29 -0600

    Tweaked example recipe

--- post_install/_recipe-example-kitchen-sink.sh
+++ post_install/_recipe-example-kitchen-sink.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # Soagi Post-Install Recipe (Kitchen Sink)
 
+BTRBK_SRC_ROOT_VOL_PATH="/mnt/btr_root_vol" 
+BTRBK_SNAPSHOT_SUBVOL_NAME="btr_snapshots"
+BTRBK_TARGET_ROOT_VOL_PATH="/run/media/robert/MyBackupHD" 
+BTRBK_BACKUP_LABEL="myLaptop"
+
 # Core applications / utilities
 ./core_yay.sh
 ./core_zsh.sh
-./core_btrbk.sh /mnt/btr_root_vol btr_snapshots /run/media/robert/MyBackupHD myLaptop
+./core_btrbk.sh $BTRBK_SRC_ROOT_VOL_PATH $BTRBK_SNAPSHOT_SUBVOL_NAME $BTRBK_TARGET_ROOT_VOL_PATH $BTRBK_BACKUP_LABEL
 
 # Web applications / utilities
 ./web_chromium.sh --enable-vaapi
 ./web_slack.sh
 ./web_teams.sh
 
+GIT_USERNAME="Robert Strube"
+GIT_EMAIL="robert@mydomain.com"
+
 # Development applications / utilities
-./dev_git.sh --install-git-credential-manager
+./dev_git.sh $GIT_USERNAME $GIT_EMAIL --install-git-credential-manager
 ./dev_vscode.sh
 
 # Media
