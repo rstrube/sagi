2cc6f2817c2d1a139210b35aa11952395b0e260a
Robert <rstrube@gmail.com>
2020-09-24 08:42:28 -0600

    Added ingredient for standardnotes

---
+++ ingredients/productivity_standardnotes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Productivity - Standard Notes
+
+DIR=$(dirname "$0")
+source $DIR/helper/_common-functions.sh
+
+function main() {
+    check_critical_prereqs
+    install
+}
+
+function install() {
+    yay -Syu --noconfirm --needed standardnotes-destkop
+}
+
+function check_critical_prereqs() {
+    check_yay_prereq
+}
+
+main "$@"
