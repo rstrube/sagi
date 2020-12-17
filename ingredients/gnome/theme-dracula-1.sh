ad21b03a759937887544058f90af7d7e51ed8a5b
Robert <rstrube@gmail.com>
2020-12-17 13:32:08 -0700

    Update the Gnome terminal color profile to make it more consistent with the Dracula theme.

--- ingredients/gnome/theme-dracula-1.sh
+++ ingredients/gnome/theme-dracula-1.sh
@@ -3,6 +3,8 @@
 #|# Gnome Dracula theme
 #|#./ingredients/gnome/theme-dracula-1.sh
 
+# Please refer to https://draculatheme.com/contribute/ for a full list of hex codes
+
 function main() {
 
     install
@@ -51,9 +53,11 @@ function configure_gnome_terminal_theme {
 
     GT_PROFILE_NAME="'Dracula'"
     GT_PROFILE_BACKGROUND_COLOR="'#282A36'"
-    GT_PROFILE_BOLD_COLOR="'#6E46A4'"
+    GT_PROFILE_BOLD_COLOR="'#BD93F9'"
     GT_PROFILE_FOREGROUND_COLOR="'#F8F8F2'"
-    GT_PROFILE_PALLETE="['#262626', '#E356A7', '#42E66C', '#E4F34A', '#9B6BDF', '#E64747', '#75D7EC', '#EFA554', '#7A7A7A', '#FF79C6', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF5555', '#8BE9FD', '#FFB86C']"
+
+    #                    Background B-Pink     B-Green    B-Yellow   B-Purple   B-Red      B-Cyan     B-Orange   Curr Line  Pink       Green      Yellow     Purple     Red        Cyan       Orange
+    GT_PROFILE_PALLETE="['#282A36', '#FF79C6', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF5555', '#8BE9FD', '#FFB86C', '#44475A', '#FF79C6', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF5555', '#8BE9FD', '#FFB86C']"
 
     dconf write ${GT_DCONF_DIR}/default "'$GT_PROFILE_ID'"
     dconf write ${GT_DCONF_DIR}/list "['$GT_PROFILE_ID']"
