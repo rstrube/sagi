4c339873db77757917b797109169a30118e308e2
Robert Strube <rstrube@gmail.com>
2021-02-20 15:15:08 -0700

    Tweaked .net ingredient to use AUR packages (more up to date)

--- ingredients/dev/ms-dotnet-core.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#|#install-pkg dotnet-runtime dotnet-sdk aspnet-targeting-pack aspnet-runtime #.NET Core runitem and SDK
-
-paru -Syu --noconfirm --needed dotnet-runtime dotnet-sdk aspnet-targeting-pack aspnet-runtime
---
+++ ingredients/dev/ms-dotnet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#|#install-pkg dotnet-runtime-bin dotnet-sdk-bin aspnet-runtime-bin aspnet-targeting-pack-bin #.NET Core runitem and SDK
+
+paru -Syu --noconfirm --needed dotnet-runtime-bin dotnet-sdk-bin aspnet-runtime-bin aspnet-targeting-pack-bin
