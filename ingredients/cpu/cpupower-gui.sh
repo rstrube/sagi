7fa24a9b741d03e6026091112da6d27cdc65f8ea
Robert <rstrube@gmail.com>
2021-03-08 09:34:33 -0700

    Updated cpupower-gui ingredient.

--- ingredients/cpu/cpupower-gui.sh
+++ ingredients/cpu/cpupower-gui.sh
@@ -2,3 +2,21 @@
 #|#install-pkg cpupower-gui #GUI utility to set CPU govenor settings
 
 paru -Syu --noconfirm --needed cpupower-gui
+
+cat <<EOT > "00-cpg.config"	
+[Profile]
+profile = Balanced
+
+[GUI]
+allcpus_default = True
+tick_marks_enabled = True
+frequency_ticks = True
+energy_pref_per_cpu = False
+EOT
+
+mkdip -p ~/.config/cpupower_gui
+cp 00-cpg.config ~/.config/cpupower_gui/
+rm 00-cpg.config
+
+# Enable the user systemd service
+systemctl --user enable cpupower-gui-user.service
