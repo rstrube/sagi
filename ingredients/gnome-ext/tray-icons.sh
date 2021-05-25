24d0f7dc904e42d5337f40e16aa47dbe9ce848a4
Robert <rstrube@gmail.com>
2021-05-24 23:32:40 -0600

    Switched tray icons ingredient to tray icons reloaded

--- ingredients/gnome-ext/tray-icons.sh
+++ ingredients/gnome-ext/tray-icons.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#|#./ingredients/gnome-ext/tray-icons.sh #Tray Icons Gnome Extension https://github.com/zhangkaizhao/gnome-shell-extension-tray-icons
+#|#./ingredients/gnome-ext/tray-icons.sh #Tray Icons Reloaded Gnome Extension https://github.com/MartinPL/Tray-Icons-Reloaded
 
-paru -S --noconfirm --needed gnome-shell-extension-tray-icons
+paru -S --noconfirm --needed gnome-shell-extension-tray-icons-reloaded-git
